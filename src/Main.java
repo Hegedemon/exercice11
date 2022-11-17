8817661db120cbf5f4b17579fd95d0bcf9fcc3d9
Hegedemon <113758660+Hegedemon@users.noreply.github.com>
2022-11-17 04:59:26 +0100

    first

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner val =new Scanner(System.in);
+        System.out.println(" entrer un nombre entier positif N");
+        int N=val.nextInt();
+        System.out.println(" entrer un nombre entier positif M");
+        int M=val.nextInt();
+        System.out.println(N+ " | " +M);
+        int n=N, m=M;
+        do {
+            if(N>M){
+               N=N-M;
+                System.out.println(N+ " | " +M);
+            }
+                if(M>N){
+                    M=M-N;
+                    System.out.println(N+ " | " +M);
+                }
+        }while(N!=M);
+        System.out.println("le PGCD de " +n+ " et " +m+ " vaut " +N);
     }
 }
